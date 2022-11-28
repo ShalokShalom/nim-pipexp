8c341a17a02e3520ef002ded50171fc8863a01f5
emanresu3 <emanresu3@noreply.codeberg.org>
2022-11-28 19:07:51 +0000

    Refactor macro with undescoredCall proc

--- src/pipexp.nim
+++ src/pipexp.nim
@@ -24,36 +24,28 @@ macro `|`*(arg, fn: untyped): untyped =
     result.insert(1, arg)
 
 
+proc underscoredCall(fn, arg0: NimNode): NimNode =
+  case fn.kind:
+  of nnkIdent:
+    result = newCall(fn, arg0)
+  of nnkCall, nnkCommand:
+    let
+      u = underscorePos(fn)
+      arg = arg0
+    result = newNimNode(nnkCall)
+      .add(fn[0])
+    for i in 1..u-1: result.add fn[i]
+    result.add(arg)
+    for i in u+1..fn.len-1: result.add fn[i]
+  of nnkStmtList, nnkStmtListExpr:
+    result = arg0
+    for stmt in fn.children:
+      result = underscoredCall(stmt, result)
+  else:
+    result = newCall(fn, arg0)
+
+
 macro pipe*(arg: untyped, fns: varargs[untyped]): untyped =
   result = arg
   for fn in fns:
-    case fn.kind:
-    of nnkIdent:
-      result = newCall(fn, result)
-    of nnkCall, nnkCommand:
-      let
-        u = underscorePos(fn)
-        arg = result
-      result = newNimNode(nnkCall)
-        .add(fn[0])
-      for i in 1..u-1: result.add fn[i]
-      result.add(arg)
-      for i in u+1..fn.len-1: result.add fn[i]
-    of nnkStmtList, nnkStmtListExpr:
-      for stmt in fn.children:
-        case stmt.kind:
-        of nnkIdent:
-          result = newCall(stmt, result)
-        of nnkCall, nnkCommand:
-          let
-            u = underscorePos(stmt)
-            arg = result
-          result = newNimNode(nnkCall)
-            .add(stmt[0])
-          for i in 1..u-1: result.add stmt[i]
-          result.add(arg)
-          for i in u+1..stmt.len-1: result.add stmt[i]
-        else:
-          result = newCall(fn, result)
-    else:
-      result = newCall(fn, result)
+    result = underscoredCall(fn, result)
